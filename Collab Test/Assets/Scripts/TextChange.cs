2c43e661a9ebf0f14989de02fc1f3bf68f61b535
jaredseale <jared.seale@gmail.com>
2023-02-16 21:15:10 -0600

    commit 1
    first test

---
+++ Collab Test/Assets/Scripts/TextChange.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextChange : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI textObj;
+
+    public void ChangeText() {
+        textObj.text = "bitche";
+    }
+
+    //get outta my scripts >_<
+
+}
